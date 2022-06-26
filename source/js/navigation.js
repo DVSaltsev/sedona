4df7c8a40adafee78280d3ebe69308d883d0df4a
DVSaltsev <dvsaltsev@gmail.com>
2022-06-26 19:14:39 +0500

    адаптив фоновые картинки до каталога

---
+++ source/js/navigation.js
@@ -0,0 +1,11 @@
+let navWrapper = document.querySelector('.header__nav-wrapper');
+let navToggle = document.querySelector('.header__nav-toggle');
+
+// navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  navWrapper.classList.toggle('header__nav--closed');
+  navWrapper.classList.toggle('header__nav--opened');
+});
+
+
